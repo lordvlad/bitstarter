c7bc1febd3d69ed27b2b4e4ff18e612e15e73f4b
lord vlad <waldemar.reusch@googlemail.com>
2013-08-26 11:59:22 +0000

    cleaned up

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
-var express = require('express'),
-port = process.env.PORT || 8080;
-os = require('os'),
-fs = require('fs'),
-app = express.createServer()
-    .use(express.logger())
-    .use(express.static(__dirname + '/static'))
+
+var express = require('express');
+var port = process.env.PORT || 8080;
+var fs = require('fs');
+var dict = require('./dgs');
+
+var app = express()
+    .use(dict)
     .use(express.compress())
+    .use(express.bodyParser())
+    .post('/subscribe', function(req, res){
+	fs.appendFile('emails.dat', req.body.email+"\n", 'utf-8', function(err){
+	    if (err){ res.end(err);}
+	    res.send("vielen Dank.<br><br><a href='/'>zurück</a>");
+	});
+    })
+    .use(express.static(__dirname + '/static'))
     .listen(port, function(){
 	console.log("listening on " + port);
     });
