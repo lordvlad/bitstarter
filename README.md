c69a0c9de10f0dde7710ec550aa516d1fc31d887
lord vlad <waldemar.reusch@googlemail.com>
2013-08-26 15:03:16 +0000

    updated to use views

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ your signs
 ==========
 
 Noch kein Gebärdensprach Wörterbuch gefunden, was dich zufrieden stellt?
+
 Dann hilf uns, ein besseres zu gestalten!
--- web.js
+++ web.js
@@ -3,16 +3,36 @@
 var express = require('express');
 var port = process.env.PORT || 8080;
 var fs = require('fs');
-var dict = require('./dgs');
+var hogan = require('hogan-express');
+var flash= require('connect-flash');
 
 var app = express()
-    .use(dict)
+    .set('view engine', 'html')
+    .set('layout', 'layout')
+    .set('views', __dirname + '/views')
+    .set('partials', {head: 'head', foot: 'foot'})
+    .enable('view cache')
+    .engine('html', hogan)
     .use(express.compress())
     .use(express.bodyParser())
+    .use(express.cookieParser('asfag4asg5as6g54asg657asg87as98g'))
+    .use(express.session({cookie: {maxAge: 60000 }}))
+    .use(flash())
+    .get('/', function(req, res){	
+	res.render('index', {
+	    title: 'your signs - deine gebärden',
+	    success: req.flash('success'),
+	    info: req.flash('info'),
+	    error: req.flash('error')
+	});
+    })
     .post('/subscribe', function(req, res){
 	fs.appendFile('emails.dat', req.body.email+"\n", 'utf-8', function(err){
-	    if (err){ res.end(err);}
-	    res.send("vielen Dank.<br><br><a href='/'>zurück</a>");
+	    if (err){
+		req.flash('error', 'Deine Email konnte nicht gespeichert werden.<br /><br />' + err)
+	    }
+	    req.flash('success', 'Vielen Dank für deine Anmeldung.');
+	    res.redirect('/');
 	});
     })
     .use(express.static(__dirname + '/static'))
